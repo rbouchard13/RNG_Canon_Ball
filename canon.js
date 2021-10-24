851fcb4a063d1980aaa6cc8110792e18f1581bc4
rbouchard13 <88938831+rbouchard13@users.noreply.github.com>
2021-10-24 19:14:41 -0400

    Adjusted Canon Fire to Mad Programmer losing it

--- canon.js
+++ canon.js
@@ -2,22 +2,22 @@
 var velX = {};
 var velY = {};
 var i = 1;
-function fireCanon() {if (i > 25){alert("You have reached the maximum number of allowable canon balls for this page!"); return;};	
+function fireCanon(){	
 	velX[i] = 3; velY[i] = -1;
     	var bgColor = "rgb(" + Math.floor(Math.random() * 256) + "," + (100 + Math.floor(Math.random() * 256)) + "," + (50 + Math.floor(Math.random() * 256)) + ")";
   		ball = document.createElement("div");
   		ball.style.backgroundColor = bgColor;
   		ball.className = "ball";
 		ball.id = "ball" + i;
-  		ball.style.height = "20px"; ball.style.width = "20px";
-  		ball.style.bottom = "80px"; ball.style.left = "118px";
+  		ball.style.height = "30px"; ball.style.width = "30px";
+  		ball.style.bottom = "135px"; ball.style.left = "180px";
   		ball.style.border= "solid";
   		document.body.appendChild(ball);i++;
 }
 function moveBall(){
 	var els = document.getElementsByClassName("ball");
 	if( els.length > 0){
-    	var edgew = window.innerWidth - 26; var edgeh = window.innerHeight -26;
+    	var edgew = window.innerWidth - 36; var edgeh = window.innerHeight -36;
 	var c; 
 	for (c = 1; c < i; c++){
 		var posX = document.getElementById("ball" + c).offsetLeft; posX = posX + velX[c]; document.getElementById("ball" + c).style.left = posX +'px';
@@ -27,4 +27,5 @@ function moveBall(){
 		}
 	}	
 }
-setInterval(moveBall, 1);
+setInterval(moveBall, 3);
+setInterval(fireCanon, 3850);
